4063ae76a72f6aa3335bca32768b3d3ed7692329
YoitsuHolo-X395 <2636124387@qq.com>
2023-09-08 02:23:14 +0800

    test: add pthread perf testwq

--- makefile
+++ makefile
@@ -1,2 +1,14 @@
-defualt :
-	@gcc main.c libco.c -Wall -Werror -o co && ./co
+co: main.c libco.c
+	gcc main.c libco.c -Wall -Werror -o co
+
+pt: tmain.c
+	gcc tmain.c -Wall -Werror -lpthread -o pt
+
+test: pt co
+	-timeout 1s ./pt > pt.txt
+	-timeout 1s ./co > co.txt
+	wc -l co.txt pt.txt
+
+clean:
+	rm pt.txt pt
+	rm co.txt co
--- tmain.c
+++ tmain.c
@@ -1,18 +1,17 @@
-#include "libco.h"
+#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-void entry(void *arg)
+void *entry(void *arg)
 {
     int c = *((int *)arg);
     // for (volatile int i = 1; i < 1000; i++)
     while (1)
     {
         printf("%d\n", c);
-        co_yield();
     }
 }
-struct co *coo[100];
+
+pthread_t pthread_id[100];
 int args[100];
 
 int main()
@@ -20,8 +19,10 @@ int main()
     for (int i = 0; i < 100; i++)
     {
         args[i] = i;
-        coo[i] = co_start("co", entry, args + i);
+        pthread_create(&pthread_id[i], NULL, entry, args + i);
     }
-    co_wait(coo[0]);
+    
+    for(int i=0; i<100; i++)
+        pthread_join(pthread_id[i], NULL);
     // printf("\nMain return\n");
 }
